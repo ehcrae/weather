35f194b1018a008de7fb839348d16aa0825ec7e5
Archie Collison <archie.collison@gmail.com>
2023-09-04 16:12:02 +1000

    Use more accurate API

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import Header from './components/Header';
 import Body from './components/Body';
 import './App.css';
 
+export const key = '';
+
 function App() {
   return (
     <div className="App">
-    <head>
-      <link rel="stylesheet" href="pico/pico.min.css"></link>
-    </head>
       < Header />
       < Body />
     </div>
